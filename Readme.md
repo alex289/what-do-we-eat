0d2ecb875f364fe20001b74f4ca59b0f7d43cd48
JuzouSatoru2 <mccr4fthd@gmail.com>
2020-10-22 20:23:57 +0200

    Add cli to add json data

--- Readme.md
+++ Readme.md
@@ -2,4 +2,6 @@
 
 > Web app to tell you what to eat
 
-Setup your own food list just by changing the food.json file. (Cli in future...)
+Use the cli to add food to the List or make your own json list.
+
+Run `node food.js --help` to get to know how to use it
--- food.js
+++ food.js
@@ -1,15 +1,53 @@
 'use strict';
 
 const fs = require('fs');
+const inquirer = require('inquirer');
+const { Command } = require('commander');
+
+const program = new Command();
+program.version(require('./package.json').version);
+program
+  .option('-a, --add', 'add new food')
+  .option('-g, --get', 'get food list');
+
+program.parse(process.argv);
 
 const rawdata = fs.readFileSync('food.json');
 const foodList = JSON.parse(rawdata);
 
-// const userInput;
+if (program.add) {
+  const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: "What's the name of the food",
+    },
+    {
+      type: 'input',
+      name: 'size',
+      message: 'What size does it has per people',
+    },
+    {
+      type: 'confirm',
+      name: 'deliverable',
+      message: 'Is it deliverable',
+    },
+    {
+      type: 'number',
+      name: 'effort',
+      message: "What's the effort to cook",
+    },
+  ];
 
-// foodList.food.push(userInput);
-// fs.writeFileSync('food.json', JSON.stringify(foodList));
+  inquirer.prompt(questions).then((answers) => {
+    foodList.food.push(answers);
+    fs.writeFileSync('food.json', JSON.stringify(foodList));
+    console.log(JSON.stringify(answers, null, '  '));
+  });
+}
 
-foodList.food.map(item => {
+if (program.get || !program.add) {
+  foodList.food.map((item) => {
     console.log(item);
-});
+  });
+}
