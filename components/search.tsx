a7f36a9f7f2a0dc1a9e587320b4f22303735cfff
Alex289 <mccr4fthd@gmail.com>
2021-11-22 16:03:32 +0100

    Add search function

---
+++ components/search.tsx
@@ -0,0 +1,22 @@
+import foodList from '@food';
+
+const Search = ({ input }: { input: string }) => {
+  return (
+    <ul className="grid xl:grid-flow-row xl:grid-cols-5 gap-5 md:grid-flow-column px-2">
+      {foodList.food.map((food, index) => {
+        if (food.name.match(input)) {
+          return (
+            <li key={index} className="max-w-md py-6 px-8 shadow-lg rounded-lg">
+              <p className="text-lg font-semibold">{food.name}</p>
+              <p>Size: {food.size}</p>
+              <p>Deliverable: {food.deliverable ? 'Yes' : 'No'}</p>
+              <p>Effort: {food.effort}</p>
+            </li>
+          );
+        }
+      })}
+    </ul>
+  );
+};
+
+export default Search;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,22 @@ import { useState } from 'react';
 import Layout from '@/components/layout';
 import Food from '@/components/food';
 import Random from '@/components/random';
+import Search from '@/components/search';
 
 export default function Index() {
   const [clicked, setClicked] = useState(false);
   const [btnTitle, setBtnTitle] = useState('Get random food');
+  const [inputText, setInputText] = useState('');
 
-  function handleClick(e: { preventDefault: () => void }) {
+  function handleClick(e: React.MouseEvent) {
     e.preventDefault();
     clicked ? setClicked(false) : setClicked(true);
     clicked ? setBtnTitle('Get random food') : setBtnTitle('Get food list');
+  }
+
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    setInputText(e.target.value);
   }
 
   return (
@@ -22,7 +29,15 @@ export default function Index() {
       >
         {btnTitle}
       </button>
-      {!clicked ? <Food></Food> : <Random></Random>}
+      <input
+        onChange={handleInput}
+        type="text"
+        placeholder="Search for food..."
+        className="ml-4 p-2 shadow appearance-none border rounded leading-tight focus:outline-none focus:shadow-outline text-gray-700 dark:bg-black dark:text-gray-300"
+      ></input>
+      {!clicked && inputText === '' ? <Food></Food> : ''}
+      {clicked && inputText === '' ? <Random></Random> : ''}
+      {inputText !== '' ? <Search input={inputText}></Search> : ''}
     </Layout>
   );
 }
