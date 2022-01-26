87ea967459ace2c816d87c80deaea1fec1f20c70
Alex289 <mccr4fthd@gmail.com>
2022-01-26 20:19:56 +0100

    Update deps

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,15 +6,20 @@ import { PrismaClient } from '@prisma/client';
 // Learn more:
 // https://pris.ly/d/help/next-js-best-practices
 
-// let prisma: PrismaClient;
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient;
+}
 
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient();
-// } else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient();
-//   }
-//   prisma = global.prisma;
-// }
+let prisma: PrismaClient;
 
-export default new PrismaClient();
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
