d2ff068d834800220cfccd42a9eeb67d4d4ce170
Alex289 <mccr4fthd@gmail.com>
2022-02-11 08:50:27 +0100

    Add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
 
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
+import NProgress from 'nprogress';
 
 import '@/styles/global.css';
+import 'nprogress/nprogress.css';
 import 'react-toastify/dist/ReactToastify.css';
+
+Router.events.on('routeChangeStart', (_url, { shallow }) => {
+  if (!shallow) {
+    NProgress.start();
+  }
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({
   Component,
