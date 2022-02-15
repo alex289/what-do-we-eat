d4128f6aae45669b34a1144eb11cba0cdf72762b
Alex289 <mccr4fthd@gmail.com>
2022-02-15 23:41:40 +0100

    Temporarily disable preact

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ module.exports = {
   swcMinify: true,
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
+      // Object.assign(config.resolve.alias, {
+      //   'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+      //   react: 'preact/compat',
+      //   'react-dom/test-utils': 'preact/test-utils',
+      //   'react-dom': 'preact/compat',
+      // });
     }
 
     return config;
