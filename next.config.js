7d015f1723042e8ecd5d57cc2e8230ff78760d20
Alex289 <mccr4fthd@gmail.com>
2022-07-08 08:52:56 +0200

    chore: :arrow_up: Update deps

--- next.config.js
+++ next.config.js
@@ -18,18 +18,18 @@ const nextConfig = withPWA({
       },
     ];
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      // Object.assign(config.resolve.alias, {
-      //   'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-      //   react: 'preact/compat',
-      //   'react-dom/test-utils': 'preact/test-utils',
-      //   'react-dom': 'preact/compat',
-      // });
-    }
+  // webpack: (config, { dev, isServer }) => {
+  //  if (!dev && !isServer) {
+  // Object.assign(config.resolve.alias, {
+  //   'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+  //   react: 'preact/compat',
+  //   'react-dom/test-utils': 'preact/test-utils',
+  //   'react-dom': 'preact/compat',
+  // });
+  // }
 
-    return config;
-  },
+  // return config;
+  // },
   images: {
     domains: ['i.pinimg.com', 'lh3.googleusercontent.com'],
   },
