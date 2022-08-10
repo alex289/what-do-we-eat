22c3ddbaf68793d602d9745908abcf6ec8e0507d
Alex289 <mccr4fthd@gmail.com>
2022-08-10 20:52:46 +0200

    chore: :arrow_up: Update deps

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ children }: { children: ReactNode }) => (
   <>
     <Meta></Meta>
     <Navbar></Navbar>
-    <main className="container mx-auto mb-6">{children}</main>
+    <main className="mx-auto mb-6">{children}</main>
   </>
 );
 
