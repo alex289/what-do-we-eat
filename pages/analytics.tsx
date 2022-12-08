4fb79fff7d0c05b8896888317510971ec33ec4c6
alex289 <mccr4fthd@gmail.com>
2022-12-08 19:40:59 +0100

    feat: Redesign analytics table

--- pages/analytics.tsx
+++ pages/analytics.tsx
@@ -57,22 +57,32 @@ const AnalyticsPage: NextPage<{
   return (
     <Layout>
       <Suspense>
-        <div className="mx-auto max-w-4xl overflow-x-auto px-4">
-          <table className="mx-auto min-w-full table-auto border-collapse border border-gray-200 bg-gray-100 shadow-lg dark:border-gray-700 dark:bg-gray-800">
-            <thead>
-              <tr className="border-b border-gray-200 bg-gray-200 dark:border-gray-700 dark:bg-gray-700">
-                <th className="py-4 px-6 text-left font-medium">Food</th>
-                <th className="py-4 px-6 text-left font-medium">Picked</th>
-                <th className="py-4 px-6 text-left font-medium">Not Picked</th>
+        <div className="relative mx-auto max-w-5xl overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+            <thead className="bg-gray-100 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="py-3 px-6">
+                  Food
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Picked
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Not Picked
+                </th>
               </tr>
             </thead>
             <tbody>
-              {memoizedData.map((item, index) => {
+              {memoizedData.map((item) => {
                 return (
                   <tr
-                    key={index}
-                    className="border-b border-gray-200 hover:bg-gray-200 dark:border-gray-700 hover:dark:bg-gray-700">
-                    <td className="py-4 px-6">{item.name}</td>
+                    key={item.name}
+                    className="border-b bg-white hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-800 hover:dark:bg-gray-600">
+                    <th
+                      scope="row"
+                      className="whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white">
+                      {item.name}
+                    </th>
                     <td className="py-4 px-6">{item.picked}</td>
                     <td className="py-4 px-6">{item.notPicked}</td>
                   </tr>
