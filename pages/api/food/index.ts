cbb680b372e3b136711342d42d76e02808fe0171
alex289 <mccr4fthd@gmail.com>
2022-12-11 20:50:54 +0100

    feat: Add api filter

--- pages/api/food/index.ts
+++ pages/api/food/index.ts
@@ -12,6 +12,22 @@ export default async function handle(
     return res.status(405).json({ message: 'Only GET method allowed' });
   }
 
-  const items = await prisma.food.findMany();
+  const orderBy = req.query.orderBy;
+  const page = parseInt(req.query.page as string) || 1;
+  const take = parseInt(req.query.take as string) || 100;
+  const search = req.query.search;
+
+  const items = await prisma.food.findMany({
+    take,
+    skip: (page - 1) * take,
+    where: {
+      name: {
+        contains: search as string,
+      },
+    },
+    orderBy: {
+      [orderBy ? 'name' : 'id']: orderBy === 'desc' ? 'desc' : 'asc',
+    },
+  });
   res.json({ status: 'success', data: items });
 }
