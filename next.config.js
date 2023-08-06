3464f2c3e124c68df5cb3cc64e96c5c5b3f8c845
alex289 <mccr4fthd@gmail.com>
2023-08-06 17:20:18 +0200

    Fix cors policy

--- next.config.js
+++ next.config.js
@@ -1,38 +1,38 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const runtimeCaching = require("next-pwa/cache");
+const runtimeCaching = require('next-pwa/cache');
 
-const withPWA = require("next-pwa")({
-	dest: "public",
-	runtimeCaching,
-	mode: "production",
-	disable: process.env.NODE_ENV !== "production",
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  runtimeCaching,
+  mode: 'production',
+  disable: process.env.NODE_ENV !== 'production',
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	async headers() {
-		return [
-			{
-				source: "/:path*",
-				headers: securityHeaders,
-			},
-		];
-	},
-	images: {
-		domains: [
-			"i.pinimg.com",
-			"lh3.googleusercontent.com",
-			"cdn.pixabay.com",
-			"www.kikko-restaurant.de",
-		],
-	},
+  reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+  images: {
+    domains: [
+      'i.pinimg.com',
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'www.kikko-restaurant.de',
+    ],
+  },
 };
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' data: https://alexanderkonietzko-analytics.vercel.app/umami.js;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' data: https://alexanderkonietzko-analytics.vercel.app/script.js;
   child-src 'self';
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
@@ -42,41 +42,41 @@ const ContentSecurityPolicy = `
 `;
 
 const securityHeaders = [
-	// https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-	{
-		key: "Content-Security-Policy",
-		value: ContentSecurityPolicy.replace(/\n/g, ""),
-	},
-	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-	{
-		key: "Referrer-Policy",
-		value: "origin-when-cross-origin",
-	},
-	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-	{
-		key: "X-Frame-Options",
-		value: "DENY",
-	},
-	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-	{
-		key: "X-Content-Type-Options",
-		value: "nosniff",
-	},
-	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-	{
-		key: "X-DNS-Prefetch-Control",
-		value: "on",
-	},
-	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-	{
-		key: "Strict-Transport-Security",
-		value: "max-age=31536000; includeSubDomains; preload",
-	},
-	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-	{
-		key: "Permissions-Policy",
-		value: "camera=(), microphone=(), geolocation=()",
-	},
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
 ];
 
 module.exports = withPWA(nextConfig);
