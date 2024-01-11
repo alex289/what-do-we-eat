8db9c879f7785f564eb35290f893d0b58a40a471
alex289 <mccr4fthd@gmail.com>
2024-01-11 21:48:58 +0100

    feat: Use env config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export default defineConfig({
-  schema: './drizzle/schema.ts',
+  schema: './src/lib/schema.ts',
   out: './drizzle',
   driver: 'mysql2',
   dbCredentials: {
