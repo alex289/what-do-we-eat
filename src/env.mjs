3008472fd952e568a87ec9113d742b4cd215beda
alex289 <mccr4fthd@gmail.com>
2024-04-24 12:45:03 +0200

    fix: Add missing env variable to ci

--- src/env.mjs
+++ src/env.mjs
@@ -4,6 +4,9 @@ import { z } from 'zod';
 export const env = createEnv({
   server: {
     POSTGRES_URL: z.string(),
+    SENTRY_AUTH_TOKEN: z.string(),
+    UPSTASH_REDIS_REST_URL: z.string(),
+    UPSTASH_REDIS_REST_TOKEN: z.string(),
     CLERK_SECRET_KEY: z.string(),
     NODE_ENV: z
       .enum(['development', 'test', 'production'])
@@ -14,9 +17,13 @@ export const env = createEnv({
   },
   runtimeEnv: {
     POSTGRES_URL: process.env.POSTGRES_URL,
+    SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     NODE_ENV: process.env.NODE_ENV,
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   emptyStringAsUndefined: true,
