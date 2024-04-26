86d7099fb13c574d7ce19bdc58613021835f029c
alex289 <mccr4fthd@gmail.com>
2024-04-26 16:41:59 +0200

    feat: Add real navigation menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,27 +1,53 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 import { currentUser } from '@clerk/nextjs/server';
-import Link from 'next/link';
+import { forwardRef } from 'react';
 
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from '@/components/ui/navigation-menu';
+import { cn } from '@/lib/utils';
 import { ThemeToggle } from './theme-toggle';
+import { Button } from './ui/button';
+
+import type { ComponentPropsWithoutRef, ElementRef } from 'react';
 
 const Navbar = async () => {
   const activeUser = await currentUser();
   const isAdmin = activeUser?.publicMetadata.admin;
   return (
     <nav className="top-0 z-50 mx-auto my-0 flex w-full items-center justify-between p-3 text-gray-900 dark:text-gray-100 md:mb-4 md:p-0">
-      <div className="flex p-1 sm:p-4 items-center gap-4">
-        <h1 className="text-lg font-semibold text-gray-900 dark:text-gray-100">
-          <Link href="/">What do we eat?</Link>
-        </h1>
-
-        <SignedIn>
-          {isAdmin ? (
-            <>
-              <Link href="/dashboard">Dashboard</Link>
-              <Link href="/analytics">Analytics</Link>
-            </>
-          ) : null}
-        </SignedIn>
+      <div className="p-1 sm:p-4">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className="text-lg">
+                What do we eat?
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[300px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                  <ListItem title="Home" href="/">
+                    The best recipes
+                  </ListItem>
+                  {isAdmin ? (
+                    <>
+                      <ListItem title="Dashboard" href="/dashboard">
+                        Dashboard for admins
+                      </ListItem>
+                      <ListItem title="Analytics" href="/analytics">
+                        Analytics for admins
+                      </ListItem>
+                    </>
+                  ) : null}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
       <div className="flex flex-row items-center gap-4 mr-4">
         <ThemeToggle />
@@ -31,6 +57,30 @@ const Navbar = async () => {
   );
 };
 
+const ListItem = forwardRef<ElementRef<'a'>, ComponentPropsWithoutRef<'a'>>(
+  ({ className, title, children, ...props }, ref) => {
+    return (
+      <li>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+              className,
+            )}
+            {...props}>
+            <div className="text-sm font-medium leading-none">{title}</div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+              {children}
+            </p>
+          </a>
+        </NavigationMenuLink>
+      </li>
+    );
+  },
+);
+ListItem.displayName = 'ListItem';
+
 function SessionToggle() {
   return (
     <>
@@ -38,7 +88,9 @@ function SessionToggle() {
         <UserButton />
       </SignedIn>
       <SignedOut>
-        <SignInButton />
+        <SignInButton mode="modal">
+          <Button>Sign in</Button>
+        </SignInButton>
       </SignedOut>
     </>
   );
