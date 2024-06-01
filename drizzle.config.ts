0039461c7d4a55830d55cab22b1e103fca26ab77
alex289 <mccr4fthd@gmail.com>
2024-06-01 12:56:06 +0200

    chore: Update deps

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import { type Config } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
 
 import { env } from './src/env.mjs';
 
-export default {
+export default defineConfig({
   schema: './src/server/db/schema.ts',
-  driver: 'pg',
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: env.POSTGRES_URL,
+    url: env.POSTGRES_URL,
   },
   tablesFilter: ['wdwe_*'],
-} satisfies Config;
+});
